8aef30bdb1dd7951a2579b2f9862e33334edb8f1
Guillem Francisco Giné <gfrancisco@obsa.ad>
2019-10-24 19:41:45 -0400

    New pois file

--- getData.sh
+++ getData.sh
@@ -11,7 +11,7 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
         wget "https://www.dropbox.com/s/ubmm9w4zg8u09ib/ActivitiesZones.geojson?dl=0" -O data/GIS/ActivitiesZones.geojson
         wget "https://www.dropbox.com/s/tdlhok3b9vleznv/buildings.geojson?dl=0" -O data/GIS/buildings.geojson
         wget "https://www.dropbox.com/s/rntbtezjmiv0lkr/Parishes.geojson?dl=0" -O data/GIS/Parishes.geojson
-        wget "https://www.dropbox.com/s/1ghj4twm8hlbb7x/POIs.geojson?dl=0" -O data/GIS/POIs.geojson
+        wget "https://www.dropbox.com/s/1aha96wrb7auvr5/pois.geojson?dl=0" -O data/GIS/POIs.geojson
 
         #Get mccmnc
         wget "https://www.dropbox.com/s/uu6cgvlozstydhj/mcc-mnc-table.csv?dl=0" -O data/mcc-mnc-table.csv
@@ -28,7 +28,7 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
         curl -L -o data/GIS/ActivitiesZones.geojson https://www.dropbox.com/s/ubmm9w4zg8u09ib/ActivitiesZones.geojson?dl=0
         curl -L -o data/GIS/buildings.geojson https://www.dropbox.com/s/tdlhok3b9vleznv/buildings.geojson?dl=0
         curl -L -o data/GIS/Parishes.geojson https://www.dropbox.com/s/rntbtezjmiv0lkr/Parishes.geojson?dl=0
-        curl -L -o data/GIS/POIs.geojson https://www.dropbox.com/s/1ghj4twm8hlbb7x/POIs.geojson?dl=0
+        curl -L -o data/GIS/POIs.geojson https://www.dropbox.com/s/1aha96wrb7auvr5/pois.geojson?dl=0
 
         #Get mccmnc
         curl -L -o data/mcc-mnc-table.csv https://www.dropbox.com/s/uu6cgvlozstydhj/mcc-mnc-table.csv?dl=0
